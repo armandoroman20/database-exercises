22ae20728f9710b10798393cf61f43907822a419
Armando Roman <armando.roman.jr20@gmail.com>
2020-07-08 12:39:14 -0500

    completed where_exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,25 +5,57 @@ select *
 from employees
 where first_name in ('Irena', 'Vidya', 'Maya');
 
--- select *
--- from employees
--- where first_name in ('Irena', 'Vidya')
--- or first_name = 'Maya';
+-- # Update your query for 'Irena', 'Vidya', or 'Maya'
+-- to use OR instead of IN — 709 rows.
+SELECT * FROM employees
+WHERE first_name = 'Irena'
+  OR first_name = 'Vidya'
+  OR first_name = 'Maya';
 
+-- # Add a condition to the previous query
+-- to find everybody with those names who is also male — 441 rows.
+SELECT * FROM employees
+WHERE first_name = 'Irena'
+  OR first_name = 'Vidya'
+  OR first_name = 'Maya'
+  AND gender = 'M';
 
 -- Find all employees whose last name starts with 'E' — 7,330 rows.
 select * from employees where last_name like 'E%'
-or last_name like '%E';
+
+-- # Find all employees whose last name
+-- starts or ends with 'E' — 30,723 rows.
+SELECT * FROM employees
+WHERE last_name LIKE 'e%'
+  OR last_name LIKE '%e';
+
+-- # Duplicate the previous query and update it to find
+-- all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
+
 
 --Find all employees hired in the 90s — 135,214 rows.
 select * from employees where hire_date between '1990-01-01' and '1999-12-31';
+-- OR
+SELECT * FROM employees WHERE hire_date LIKE '199%';
 
 -- Find all employees born on Christmas — 842 rows.
--- select * from employees where birth_date = '1954-12-25';
+SELECT * FROM employees
+WHERE birth_date LIKE '%12-25';
+
+-- # Find all employees hired in the 90s and born on Christmas — 362 rows.
+SELECT * FROM employees
+WHERE hire_date LIKE '199%'
+  AND birth_date LIKE '%12-25';
 
 -- Find all employees with a 'q' in their last name — 1,873 rows.
 select * from employees where last_name like '%q%';
 
-select * from employees where last_name = '%q%'
-and last_name not 'qu';
+-- # Find all employees with a 'q' in their last name
+-- but not 'qu' — 547 rows.
+SELECT * FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
 
