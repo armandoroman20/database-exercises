3dc5b93fb89b4cd59072dba38f953c6ab27dcc8a
Armando Roman <armando.roman.jr20@gmail.com>
2020-07-08 12:58:25 -0500

    completed mysql clauses exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,34 +3,68 @@ use employees;
 select *
 from employees
 where first_name in ('Irena', 'Vidya', 'Maya')
+and gender = 'm'
 order by first_name;
+
+-- # Update the query to order by first name and then last name.
+-- The first result should now be Irena Acton
+-- and the last should be Vidya Zweizig.
 
 select *
 from employees
 where first_name in ('Irena', 'Vidya', 'Maya')
+and gender = 'm'
 order by first_name, last_name;
 
 
-select *
-from employees
-where first_name in ('Irena', 'Vidya', 'Maya')
-order by last_name, first_name desc;
+-- # Change the order by clause so that you order by last name
+-- before first name. Your first result
+-- should still be Irena Acton but now the last result should be Maya Zyda.
+
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+AND gender = 'M'
+ORDER BY last_name, first_name;
 
 
--- Find all employees whose last name starts with 'E' — 7,330 rows.
-select * from employees where last_name like 'E%'
-or last_name like '%E'
-order by emp_no desc;
+-- # Update your queries for employees with 'E' in their last name
+-- to sort the results by their employee number.
+-- Your results should not change!
 
---Find all employees hired in the 90s — 135,214 rows.
-select * from employees where hire_date between '1990-01-01' and '1999-12-31';
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E'
+ORDER BY emp_no;
 
--- Find all employees born on Christmas — 842 rows.
--- select * from employees where birth_date = '1954-12-25';
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+ORDER BY emp_no;
 
--- Find all employees with a 'q' in their last name — 1,873 rows.
-select * from employees where last_name like '%q%';
 
--- Change the query for employees hired in the 90s
--- and born on Christmas such that the first result is the oldest employee who was hired last.
--- It should be Khun Bernini.
+-- # Change the query for employees hired in the 90s
+-- and born on Christmas such that the first result is
+-- the oldest employee who was hired last. It should be Khun Bernini.
+
+SELECT * FROM employees WHERE hire_date > '199%'
+AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date DESC;
+
+
+
+SELECT * FROM employees WHERE hire_date > '199%';
+
+SELECT * FROM employees WHERE hire_date LIKE '%12-25';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees WHERE last_name NOT LIKE '%qu%'
+AND last_name LIKE '%q%';
+
+-- Create a new file called order_by_exercises.sql and copy in the contents of your where_exercises.sql.
+-- Modify your first query to order by first name. The first result should be Irena Reutenauer and the last result should be Vidya Simmen.
+-- Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
+-- Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+-- Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
+-- Now reverse the sort order for both queries.
+-- Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
